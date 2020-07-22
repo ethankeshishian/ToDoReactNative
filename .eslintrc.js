d0351132b6c20dd0ba1eca8f499c5a1f54afda6d
Ethan Keshishian <ehk2000@sbcglobal.net>
2020-07-21 17:26:10 -0700

    Base code working, no redux. TODO: complete swipe

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,9 @@ module.exports = {
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    "prettier/prettier": ["error", {
+     "endOfLine":"auto"
+   }],
+  }
 };
--- App.tsx
+++ App.tsx
@@ -8,36 +8,178 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
-  View,
   Text,
   StatusBar,
+  TextInput,
+  Button,
+  View,
+  Animated,
+  TouchableHighlight,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {SwipeListView} from 'react-native-swipe-list-view';
 
-declare const global: {HermesInternal: null | {}};
+// import // Header,
+// // LearnMoreLinks,
+// // Colors,
+// // DebugInstructions,
+// // ReloadInstructions,
+// 'react-native/Libraries/NewAppScreen';
+
+// declare const global: {HermesInternal: null | {}};
 
 const App = () => {
+  function Submit(props: any) {
+    const [item, setItem] = useState("I can't edit this text?");
+
+    // function handleSubmit(e: any) {
+    //   if (item !== '') {
+    //     props.handleSubmit(item);
+    //     setItem('');
+    //   }
+    //   e.preventDefault();
+    // }
+    function handleSubmit() {
+      if (item !== '') {
+        props.handleSubmit(item);
+        setItem('');
+      }
+    }
+
+    // function handleItemState(e: any) {
+    //   setItem(e.target.value);
+    // }
+    // function handleItemState(newText: string) {
+    //   setItem(newText);
+    // }
+
+    return (
+      <View>
+        <TextInput
+          style={styles.addItem}
+          onChangeText={(text) => setItem(text)}
+          value={item}
+          onSubmitEditing={handleSubmit}
+        />
+        <Button title="Add" onPress={handleSubmit} />
+      </View>
+    );
+  }
+
+  function Items(props: any) {
+    const listData = props.data;
+    const renderItem = (data: {
+      item: {value: string};
+      index: number;
+      separators: object;
+    }) => (
+      <Animated.View>
+        <TouchableHighlight style={styles.rowFront} underlayColor={'#AAA'}>
+          <View>
+            <Text>{data.item.value}</Text>
+          </View>
+        </TouchableHighlight>
+      </Animated.View>
+    );
+    return (
+      <>
+        <SwipeListView
+          data={listData}
+          renderItem={renderItem}
+          renderHiddenItem={() => (
+            <View style={styles.rowBack}>
+              <View style={[styles.backRightBtn, styles.backRightBtnRight]}>
+                <Text style={styles.backTextWhite}>Delete</Text>
+              </View>
+            </View>
+          )}
+        />
+      </>
+    );
+  }
+
+  //   const renderItem = (data: any) => (
+  //     <Animated.View>
+  //       {/* style={[
+  //       //   styles.rowFrontContainer,
+  //       //   {
+  //       //     height: rowTranslateAnimatedValues[data.item.key].interpolate({
+  //       //       inputRange: [0, 1],
+  //       //       outputRange: [0, 50],
+  //       //     }),
+  //       //   },
+  //       // ]}> */}
+  //       <TouchableHighlight
+  //         onPress={() => console.log('You touched me')}
+  //         style={styles.rowFront}
+  //         underlayColor={'#AAA'}>
+  //         <View>
+  //           <Text>I am {data.item.text} in a SwipeListView</Text>
+  //         </View>
+  //       </TouchableHighlight>
+  //     </Animated.View>
+  //   );
+
+  //   const renderHiddenItem = () => (
+  //     <View style={styles.rowBack}>
+  //       <View style={[styles.backRightBtn, styles.backRightBtnRight]}>
+  //         <Text style={styles.backTextWhite}>Delete</Text>
+  //       </View>
+  //     </View>
+  //   );
+
+  //   return (
+  //     <View style={styles.container}>
+  //       <SwipeListView
+  //         disableRightSwipe
+  //         data={listData}
+  //         renderItem={renderItem}
+  //         renderHiddenItem={renderHiddenItem}
+  //         rightOpenValue={-Dimensions.get('window').width}
+  //         // onSwipeValueChange={onSwipeValueChange}
+  //         useNativeDriver={false}
+  //       />
+  //     </View>
+  //   );
+  // }
+
+  function MyListContainer() {
+    const [list, setList] = useState([
+      {value: 'Wash the dishes'},
+      {value: 'Make my bed'},
+    ]);
+
+    function addItem(item: any) {
+      const newItem = {value: item};
+      setList([...list, newItem]);
+    }
+
+    return (
+      <>
+        <Items data={list} />
+        <Submit handleSubmit={addItem} />
+      </>
+    );
+  }
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
+      <SafeAreaView style={styles.mainView}>
+        <Text style={styles.title}>To-Do List:</Text>
+        <ScrollView style={styles.scrollView}>
+          {/* // contentInsetAdjustmentBehavior="automatic" */}
+        </ScrollView>
+        <MyListContainer />
+        {/* <Header /> */}
+
+        {/* <button onSubmit={handleSubmit} /> */}
+        {/* {global.HermesInternal == null ? null : (
             <View style={styles.engine}>
               <Text style={styles.footer}>Engine: Hermes</Text>
             </View>
@@ -69,50 +211,95 @@ const App = () => {
               </Text>
             </View>
             <LearnMoreLinks />
-          </View>
-        </ScrollView>
+          </View> */}
       </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  mainView: {
+    display: 'flex',
   },
-  engine: {
+  title: {
+    textAlign: 'center',
+    fontSize: 32,
+  },
+  scrollView: {},
+  addItem: {
+    // justifyContent: 'flex-end',
+    // alignContent: 'flex-end',
+  },
+
+  //from swipe list container
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  backTextWhite: {
+    color: '#FFF',
+  },
+  rowFront: {
+    alignItems: 'center',
+    backgroundColor: '#CCC',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    justifyContent: 'center',
+    height: 50,
+  },
+  rowBack: {
+    alignItems: 'center',
+    backgroundColor: 'red',
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 15,
+  },
+  backRightBtn: {
+    alignItems: 'center',
+    bottom: 0,
+    justifyContent: 'center',
     position: 'absolute',
+    top: 0,
+    width: 75,
+  },
+  backRightBtnRight: {
+    backgroundColor: 'red',
     right: 0,
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  // engine: {
+  //   position: 'absolute',
+  //   right: 0,
+  // },
+  // body: {
+  //   backgroundColor: Colors.white,
+  // },
+  // sectionContainer: {
+  //   marginTop: 32,
+  //   paddingHorizontal: 24,
+  // },
+  // sectionTitle: {
+  //   fontSize: 24,
+  //   fontWeight: '600',
+  //   color: Colors.black,
+  // },
+  // sectionDescription: {
+  //   marginTop: 8,
+  //   fontSize: 18,
+  //   fontWeight: '400',
+  //   color: Colors.dark,
+  // },
+  // highlight: {
+  //   fontWeight: '700',
+  // },
+  // footer: {
+  //   color: Colors.dark,
+  //   fontSize: 12,
+  //   fontWeight: '600',
+  //   padding: 4,
+  //   paddingRight: 12,
+  //   textAlign: 'right',
+  // },
 });
 
 export default App;
